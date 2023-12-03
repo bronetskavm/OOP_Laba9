d69acde5a38114d607a19335b6074e3993427abe
Victoria Bronetska <bronetskavm@gmail.com>
2023-12-03 22:40:43 +0200

    first commit

---
+++ Solution1/Program.cs
@@ -0,0 +1,88 @@
+﻿// Введення даних для циліндра
+Console.WriteLine("Enter the parameters for the cylinder:");
+Console.Write("Enter the radius: ");
+double cylinderRadius = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the height: ");
+double cylinderHeight = Convert.ToDouble(Console.ReadLine());
+
+// Створення об'єкта циліндра та виведення площі поверхні
+Cylinder cylinder = new Cylinder(cylinderRadius, cylinderHeight);
+Console.WriteLine($"Surface Area of Cylinder: {cylinder.GetSurfaceArea()}");
+
+// Введення даних для куба
+Console.WriteLine("\nEnter the parameters for the cube:");
+Console.Write("Enter the side length: ");
+double cubeSideLength = Convert.ToDouble(Console.ReadLine());
+
+// Створення об'єкта куба та виведення площі поверхні
+Cube cube = new Cube(cubeSideLength);
+Console.WriteLine($"Surface Area of Cube: {cube.GetSurfaceArea()}");
+
+// Введення даних для конуса
+Console.WriteLine("\nEnter the parameters for the cone:");
+Console.Write("Enter the radius: ");
+double coneRadius = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the height: ");
+double coneHeight = Convert.ToDouble(Console.ReadLine());
+
+// Створення об'єкта конуса та виведення площі поверхні
+Cone cone = new Cone(coneRadius, coneHeight);
+Console.WriteLine($"Surface Area of Cone: {cone.GetSurfaceArea()}");
+public interface ITrigonometricFigure
+{
+    double GetSurfaceArea();
+}
+
+public abstract class TrigonometricFigure : ITrigonometricFigure
+{
+    public abstract double GetSurfaceArea();
+}
+
+public class Cylinder : TrigonometricFigure
+{
+    public double Radius { get; set; }
+    public double Height { get; set; }
+
+    public Cylinder(double radius, double height)
+    {
+        Radius = radius;
+        Height = height;
+    }
+
+    public override double GetSurfaceArea()
+    {
+        return 2 * Math.PI * Radius * (Radius + Height);
+    }
+}
+
+public class Cube : TrigonometricFigure
+{
+    public double SideLength { get; set; }
+
+    public Cube(double sideLength)
+    {
+        SideLength = sideLength;
+    }
+
+    public override double GetSurfaceArea()
+    {
+        return 6 * Math.Pow(SideLength, 2);
+    }
+}
+
+public class Cone : TrigonometricFigure
+{
+    public double Radius { get; set; }
+    public double Height { get; set; }
+
+    public Cone(double radius, double height)
+    {
+        Radius = radius;
+        Height = height;
+    }
+
+    public override double GetSurfaceArea()
+    {
+        return Math.PI * Radius * (Radius + Math.Sqrt(Math.Pow(Height, 2) + Math.Pow(Radius, 2)));
+    }
+}
---
+++ Solution2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Document : IComparable<Document>
+{
+    public string Title { get; set; }
+    public int Pages { get; set; }
+    public int Importance { get; set; }
+
+    public Document(string title, int pages, int importance)
+    {
+        Title = title;
+        Pages = pages;
+        Importance = importance;
+    }
+
+    public int CompareTo(Document other)
+    {
+        return Pages.CompareTo(other.Pages);
+    }
+}
+
+public class ImportanceComparer : IComparer<Document>
+{
+    public int Compare(Document x, Document y)
+    {
+        // Порівнюємо за важливістю, а якщо важливість однакова - за кількістю сторінок
+        int importanceComparison = x.Importance.CompareTo(y.Importance);
+        return (importanceComparison == 0) ? x.Pages.CompareTo(y.Pages) : importanceComparison;
+    }
+}
+
+public class DocumentCollection : IEnumerable<Document>
+{
+    private List<Document> documents = new List<Document>();
+
+    public void AddDocument(Document document)
+    {
+        documents.Add(document);
+    }
+
+    public IEnumerator<Document> GetEnumerator()
+    {
+        // Сортуємо за кількістю сторінок перед ітерацією
+        documents.Sort();
+        return documents.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Приклад використання
+        Document doc1 = new Document("Document A", 20, 3);
+        Document doc2 = new Document("Document B", 15, 2);
+        Document doc3 = new Document("Document C", 25, 1);
+
+        DocumentCollection documentCollection = new DocumentCollection();
+        documentCollection.AddDocument(doc1);
+        documentCollection.AddDocument(doc2);
+        documentCollection.AddDocument(doc3);
+
+        foreach (var doc in documentCollection)
+        {
+            Console.WriteLine($"Title: {doc.Title}, Pages: {doc.Pages}, Importance: {doc.Importance}");
+        }
+    }
+}
